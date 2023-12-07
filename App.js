762f227da4473148162fde44e23b5fd3870a9d82
Rajan Sharma <rajanraj8979@gmail.com>
2023-12-07 14:42:02 +0530

    first commit

--- App.js
+++ App.js
@@ -1,53 +1,8 @@
-import React, { useState } from 'react';
-import { View, TextInput, FlatList, Text, StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
 
-const SearchBarExample = () => {
-  const [query, setQuery] = useState('');
-  const [data, setData] = useState([
-    { id: '1', title: 'Apple' },
-    { id: '2', title: 'Banana' },
-    { id: '3', title: 'Cherry' },
-    // ... more data
-  ]);
+export default function App() {
+	return <AppNavigator />;
+}
 
-  const handleSearch = (text) => {
-    // Filter the data based on the search query
-    const filteredData = data.filter((item) =>
-      item.title.toLowerCase().includes(text.toLowerCase())
-    );
-    setQuery(text);
-    setData(filteredData);
-  };
-
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Search..."
-        onChangeText={handleSearch}
-        value={query}
-      />
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
-      />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 16,
-    paddingHorizontal: 8,
-  },
-});
-
-export default SearchBarExample;
